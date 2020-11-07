9642f9cb841084cc4d1ef562b60b7b0a2d02e71a
JMJ Riyana <jeemariyana@gmail.com>
2020-11-07 13:23:16 +0530

    d

--- chartjs/src/App.js
+++ chartjs/src/App.js
@@ -1,11 +1,13 @@
 import React from'react';
 import BarChart from'./components/BarChart';
+import LineChart from'./components/LineChart';
 import './App.css';
 
 const App = () => {
   return (
     <div>
         <BarChart />
+        <LineChart />
     </div>
   )
 }
