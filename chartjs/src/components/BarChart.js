ce0ad27720869d9eab99b58c54b8a0de5c6e2b1f
JMJ Riyana <jeemariyana@gmail.com>
2020-11-11 19:37:42 +0530

    d

--- chartjs/src/components/BarChart.js
+++ chartjs/src/components/BarChart.js
@@ -43,3 +43,68 @@ const BarChart = () => {
 }
 
 export default BarChart;
+
+/*
+
+//import ReactDOM from 'react-dom';
+
+import React from 'react';
+import { Bar } from 'react-chartjs-2'
+import logs from './NAT64_1_20.json'
+//const FileSystem = require("fs");
+//const source = FileSystem.readFile('NAT64_1_20.json');
+
+const BarChart = () => {
+    return <div>
+        <Bar
+            //const sampleJSON = JSON.parse(JSON_source)
+            
+            data={{
+                
+                labels: [logs.map.Length],            
+                datasets: [{
+                    label: '# of Votes',
+                    data: [logs.map.Time]//,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                },
+               {
+                    label: 'Quantity',
+                    data: [20,25,10,0,5,12],
+                    backgroundColor: 'light blue',
+                    borderColor: 'red'
+                }
+                ], 
+            }}
+            height={400}
+            width={600}
+            options={{ maintainAspectRatio: false }}
+        />
+    </div>
+}
+
+consr newdata = data.map((data) => {
+    return (
+        <Card key ={data.zzzzzz}>
+            <Card.Body>
+                <Card.Title>{data.info}</Card.Title>
+            </Card.Body>
+        </Card>
+    )
+})
+export default BarChart;*/
