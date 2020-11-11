9ed522846aaae8d1b8373c67f0be7a007216e286
JMJ Riyana <jeemariyana@gmail.com>
2020-11-11 21:22:28 +0530

    d

--- chartjs/src/App.js
+++ chartjs/src/App.js
@@ -1,25 +1,25 @@
 import React from'react';
 import BarChart from'./components/BarChart';
-import LineChart from'./components/LineChart';
+/*import LineChart from'./components/LineChart';
 import RadarChart from'./components/RadarChart';
 import PieChart from'./components/PieChart';
 import DoughnutChart from'./components/DoughnutChart';
 import PolarAreaChart from'./components/PolarAreaChart';
 import BubbleChart from'./components/BubbleChart';
-import ScatterChart from'./components/ScatterChart';
+import ScatterChart from'./components/ScatterChart';*/
 import './App.css';
 
 const App = () => {
   return (
     <div>
         <BarChart />
-        <LineChart />
+        {/*<LineChart />
         <RadarChart />
         <PieChart />
         <DoughnutChart />
         <PolarAreaChart />
         <BubbleChart />
-        <ScatterChart />
+        <ScatterChart />*/}
         </div>
   )
 }
--- chartjs/src/components/BarChart.js
+++ chartjs/src/components/BarChart.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import { Bar } from 'react-chartjs-2'
 
 const BarChart = () => {
@@ -42,69 +42,33 @@ const BarChart = () => {
     </div>
 }
 
-export default BarChart;
+export default BarChart;*/
 
-/*
 
-//import ReactDOM from 'react-dom';
 
 import React from 'react';
 import { Bar } from 'react-chartjs-2'
-import logs from './NAT64_1_20.json'
-//const FileSystem = require("fs");
-//const source = FileSystem.readFile('NAT64_1_20.json');
+import Data from  './data/data.json'
 
 const BarChart = () => {
-    return <div>
-        <Bar
-            //const sampleJSON = JSON.parse(JSON_source)
-            
-            data={{
-                
-                labels: [logs.map.Length],            
-                datasets: [{
-                    label: '# of Votes',
-                    data: [logs.map.Time]//,
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                },
-               {
-                    label: 'Quantity',
-                    data: [20,25,10,0,5,12],
-                    backgroundColor: 'light blue',
-                    borderColor: 'red'
-                }
-                ], 
-            }}
-            height={400}
-            width={600}
-            options={{ maintainAspectRatio: false }}
-        />
-    </div>
-}
+    Data.map( ( Data) =>{
+        return  (
+            <div>
+                <Bar
+                    data={{
+                        labels:[Data.Length],            
+                        datasets: [{
+                            label: '# of Votes',
+                            data: [Data.Time]
+                        }], 
+                    }}
+                    height={400}
+                    width={600}
+                    options={{ maintainAspectRatio: false }}
+                />
+            </div>
+        )
+    }
+)}
 
-consr newdata = data.map((data) => {
-    return (
-        <Card key ={data.zzzzzz}>
-            <Card.Body>
-                <Card.Title>{data.info}</Card.Title>
-            </Card.Body>
-        </Card>
-    )
-})
-export default BarChart;*/
+export default BarChart;
